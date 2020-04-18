87a783c98a09fbcdb4b53d48a3fa70a36d9b9569
Kingmercy-Rayne <kingrayne66@gmail.com>
2020-04-18 11:02:19 +0800

    Add JWT to incoming Login POST request

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(volleyball);
 app.use(express.json());
 
@@ -19,6 +19,16 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', auth);
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started listening on port ${port}`);
--- src/auth/index.js
+++ src/auth/index.js
@@ -14,6 +14,33 @@ const registerSchema = Joi.object().keys({
   email: Joi.string().email({ minDomainSegments: 2 }),
 });
 
+const loginSchema = Joi.object().keys({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+});
+
+function responseError(res, next, statusCode) {
+  res.status(statusCode);
+  const error = new Error("Sorry, Request can't be processed");
+  next(error);
+}
+
+function createToken_sendResponse(res, next, user) {
+  jwt.sign(
+    { _id: user._id },
+    process.env.TOKEN_SECRET,
+    { expiresIn: '1d' },
+    (err, token) => {
+      if (err) {
+        next(err);
+      } else {
+        res.json({ user, token });
+      }
+    }
+  );
+}
+
+//Routes
 router.get('/signup', (req, res, next) => {
   res.send('Welcome to the Signup route');
 });
@@ -45,18 +72,7 @@ router.post('/signup', (req, res, next) => {
                 .insert(newUser)
                 .then((addedUser) => {
                   delete addedUser.password;
-                  jwt.sign(
-                    { _id: addedUser._id },
-                    process.env.TOKEN_SECRET,
-                    { expiresIn: '1d' },
-                    (err, token) => {
-                      if (err) {
-                        next(err);
-                      } else {
-                        res.json({ addedUser, token });
-                      }
-                    }
-                  );
+                  createToken_sendResponse(re, next, addedUser);
                 })
                 .catch((eror) => next(error));
             })
@@ -71,8 +87,38 @@ router.post('/signup', (req, res, next) => {
 router.get('/login', (req, res) => {
   res.send('Yo! This is the login route');
 });
-router.post('/login', (req, res) => {
-  res.json(req.body);
+
+router.post('/login', (req, res, next) => {
+  // validate rather than validateAsync since the ...
+  const result = loginSchema.validate(req.body);
+  if (!result.error) {
+    users
+      .findOne({
+        username: req.body.username,
+      })
+      .then((user) => {
+        if (user) {
+          bcrypt.compare(req.body.password, user.password).then((result) => {
+            if (result === true) {
+              const payload = {
+                _id: user._id,
+                firstname: user.firstname,
+                username: user.username,
+                email: user.email,
+              };
+              delete user.password;
+              createToken_sendResponse(res, next, payload);
+            } else {
+              responseError(res, next, 422);
+            }
+          });
+        } else {
+          responseError(res, next, 422);
+        }
+      });
+  } else {
+    responseError(res, next, 422);
+  }
 });
 
 module.exports = router;
